7c84361e47e3842b91ebf4d848ea436cfc7c04d2
Matthias Walter <matthias@matthiaswalter.org>
2024-07-07 12:19:36 +0200

    Updated script for generating all instances.

--- all.py
+++ all.py
@@ -1,9 +1,13 @@
 import os
 import math
 
-generateLABS = False
-generateIRR = False
+generateLABS = True
+generateIRR = True
 generateHDR = True
+
+def syscall(command):
+  print(command)
+  os.system(command)
 
 if generateLABS:
   LABS_DIR='labs'
@@ -12,7 +16,7 @@ if generateLABS:
   for N in range(15,51,5):
     for factor in [8/8, 7/8, 6/8, 5/8, 4/8, 3/8, 2/8, 1/8]:
       R = math.ceil(factor * N)
-      os.system(f'python low-auto-correlation-binary.py {N} {R} {LABS_DIR}/low-auto-correlation-binary-sequences-{N:02}-{R:02}')
+      syscall(f'python low-auto-correlation-binary.py {N} {R} {LABS_DIR}/low-auto-correlation-binary-sequences-{N:02}-{R:02}')
 
 if generateIRR:
   IRR_DIR='irr'
@@ -29,16 +33,17 @@ if generateIRR:
           repetitions = [1, 2]
           pertDesc = f'{pert[0]}{pert[1]}'
         for rep in repetitions:
-          os.system(f'python image-restoration-random.py {size[0]} {size[1]} {base} {pert[0]} {pert[1]} {IRR_DIR}/image-restoration-random-{base}-{pertDesc}-{size[0]}x{size[1]}-#{rep}')
+          syscall(f'python image-restoration-random.py {size[0]} {size[1]} {base} {pert[0]} {pert[1]} {IRR_DIR}/image-restoration-random-{base}-{pertDesc}-{size[0]}x{size[1]}-#{rep}')
 
 if generateHDR:
   HDR_DIR='hdr'
   if not os.path.exists(HDR_DIR):
     os.mkdir(HDR_DIR)
-  repetitions = list(range(1,6))
+  num_repetitions = 5
+  repetitions = list(range(1, num_repetitions+1))
   for n in [50, 100, 200]:
     for m in [5*n, 10*n, 20*n]:
       for d in [2, 3, 4]:
         for r in repetitions:
-          os.system(f'python high-degree-random.py {n} {m} {d} {HDR_DIR}/high-degree-random-{n}-{m}-{d}-#{r}')
+          syscall(f'python high-degree-random.py {n} {m} {d} {HDR_DIR}/high-degree-random-{n}-{m}-{d}-#{r}')
  
